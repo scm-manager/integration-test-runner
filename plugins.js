d0f7c7ae07f798417e5b398878501fda09b737f1
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2021-04-14 14:44:05 +0200

    fix typo and remove debug logs

--- plugins.js
+++ plugins.js
@@ -2,16 +2,11 @@ const browserify = require("@cypress/browserify-preprocessor");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 const resolve = require("resolve");
 
-console.log("HELLO PLUGINS 2");
-
 module.exports = (on, config) => {
-  console.log("HELLO PLUGINS 3");
   const options = {
     ...browserify.defaultOptions,
     typescript: resolve.sync("typescript", { baseDir: config.projectRoot })
   };
-
-  console.log("HELLO PLUGINS 4");
 
   on("file:preprocessor", cucumber(options));
 };
